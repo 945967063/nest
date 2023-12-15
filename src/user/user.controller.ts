5ad40934109e234631c84cabd75b8343a5f46b2d
linian <945967063@qq.com>
2023-12-15 15:08:28 +0800

    add: 删除

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,7 @@ import {
   Get,
   Query,
   UnauthorizedException,
+  DefaultValuePipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
@@ -14,6 +15,11 @@ import { RedisService } from '../redis/redis.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { RequireLogin, UserInfo } from 'src/custom.decorator';
+import { UserDetailVo } from './vo/user-info.vo';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { generateParseIntPipe } from 'src/utils';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -48,7 +54,7 @@ export class UserController {
       subject: '注册验证码',
       html: `<p>你的注册验证码是 ${code}</p>`,
     });
-    return '发送成功';
+    return true;
   }
 
   /**登录 */
@@ -78,11 +84,7 @@ export class UserController {
       },
     );
 
-    return {
-      data: vo,
-      code: 200,
-      message: '登录成功',
-    };
+    return vo;
   }
 
   @Get('refresh')
@@ -126,5 +128,99 @@ export class UserController {
     } catch (e) {
       throw new UnauthorizedException('token 已失效，请重新登录');
     }
+  }
+
+  /**获取用户信息 */
+  @Get('info')
+  @RequireLogin()
+  async info(@UserInfo('userId') userId: number) {
+    const user = await this.userService.findUserDetailById(userId);
+
+    const vo = new UserDetailVo();
+    vo.id = user.id;
+    vo.email = user.email;
+    vo.username = user.userName;
+    vo.headPic = user.headPic;
+    vo.phoneNumber = user.phoneNumber;
+    vo.nickName = user.nickName;
+    vo.createTime = user.createTime;
+    vo.isFrozen = user.isFrozen;
+
+    return vo;
+  }
+
+  /**修改M密码 */
+  @Post(['update_password'])
+  @RequireLogin()
+  async updatePassword(
+    @UserInfo('userId') userId: number,
+    @Body() passwordDto: UpdateUserPasswordDto,
+  ) {
+    return await this.userService.updatePassword(userId, passwordDto);
+  }
+  /**更新密码获取验证码 */
+  @Get('update_password/captcha')
+  async updatePasswordCaptcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_password_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改密码验证码',
+      html: `<p>你的更改密码验证码是 ${code}</p>`,
+    });
+    return '发送成功';
+  }
+
+  @Post(['update'])
+  @RequireLogin()
+  async update(
+    @UserInfo('userId') userId: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.userService.update(userId, updateUserDto);
+  }
+
+  /**冻结用户 */
+  @Get('freeze')
+  async freeze(@Query('id') userId: number) {
+    await this.userService.freezeUserById(userId);
+    return 'success';
+  }
+
+  /**用户列表 */
+  @Get('list')
+  async list(
+    @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo'))
+    pageNo: number,
+    @Query(
+      'pageSize',
+      new DefaultValuePipe(2),
+      generateParseIntPipe('pageSize'),
+    )
+    pageSize: number,
+    @Query('username') username: string,
+    @Query('nickName') nickName: string,
+    @Query('email') email: string,
+  ) {
+    return await this.userService.findUsers(
+      username,
+      nickName,
+      email,
+      pageNo,
+      pageSize,
+    );
+  }
+
+  /**删除用户 */
+  @Get('delete')
+  async delete(@Query('id') userId: number) {
+    await this.userService.delete(userId);
+    return 'success';
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,13 +8,15 @@ import {
 import { RegisterUserDto } from './dto/register-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { RedisService } from '../redis/redis.service';
 import { md5 } from '../utils';
 import { Role } from './entities/role.entity';
 import { Permission } from './entities/permission.entity';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoginUserVo } from './vo/login-user.vo';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UserService {
   @Inject(RedisService)
@@ -40,13 +42,16 @@ export class UserService {
     if (captcha !== user.captcha) {
       throw new HttpException('验证码错误', HttpStatus.BAD_REQUEST);
     }
-
     const foundUser = await this.userRepository.findOneBy({
       userName: user.userName,
     });
 
     if (foundUser) {
       throw new HttpException('用户名已存在', HttpStatus.BAD_REQUEST);
+    }
+
+    if (foundUser.email === user.email) {
+      throw new HttpException('邮箱已被注册', HttpStatus.BAD_REQUEST);
     }
 
     const newUser = new User();
@@ -97,6 +102,9 @@ export class UserService {
     if (user.passWord !== md5(LoginUserDto.passWord)) {
       throw new HttpException('密码错误', HttpStatus.BAD_REQUEST);
     }
+    if (user.isFrozen) {
+      throw new HttpException('该用户已被冻结', HttpStatus.BAD_REQUEST);
+    }
     const vo = new LoginUserVo();
     vo.userInfo = {
       id: user.id,
@@ -112,7 +120,7 @@ export class UserService {
       permissions: user.roles.reduce((arr, item) => {
         item.permissions.forEach((permission) => {
           if (arr.indexOf(permission) === -1) {
-            arr.push(permission.code);
+            arr.push(permission);
           }
         });
         return arr;
@@ -145,5 +153,138 @@ export class UserService {
         return arr;
       }, []),
     };
+  }
+
+  async findUserDetailById(userId: number) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    return user;
+  }
+
+  async updatePassword(userId: number, passwordDto: UpdateUserPasswordDto) {
+    const captcha = await this.redisService.get(
+      `update_password_captcha_${passwordDto.email}`,
+    );
+
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if (passwordDto.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.userRepository.findOneBy({
+      id: userId,
+    });
+
+    foundUser.passWord = md5(passwordDto.password);
+
+    try {
+      await this.userRepository.save(foundUser);
+      return '密码修改成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '密码修改失败';
+    }
+  }
+
+  async update(userId: number, updateUserDto: UpdateUserDto) {
+    const foundUser = await this.userRepository.findOneBy({
+      id: userId,
+    });
+
+    if (updateUserDto.nickName) {
+      foundUser.nickName = updateUserDto.nickName;
+    }
+    if (updateUserDto.headPic) {
+      foundUser.headPic = updateUserDto.headPic;
+    }
+
+    try {
+      await this.userRepository.save(foundUser);
+      return '用户信息修改成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '用户信息修改成功';
+    }
+  }
+
+  async freezeUserById(id: number) {
+    const user = await this.userRepository.findOneBy({
+      id,
+    });
+
+    user.isFrozen = !user.isFrozen;
+
+    await this.userRepository.save(user);
+  }
+
+  async findUsers(
+    username: string,
+    nickName: string,
+    email: string,
+    pageNo: number,
+    pageSize: number,
+  ) {
+    const skipCount = (pageNo - 1) * pageSize;
+
+    const condition: Record<string, any> = {};
+
+    if (username) {
+      condition.username = Like(`%${username}%`);
+    }
+    if (nickName) {
+      condition.nickName = Like(`%${nickName}%`);
+    }
+    if (email) {
+      condition.email = Like(`%${email}%`);
+    }
+    //管理员账号不返回
+    condition.isAdmin = false;
+
+    const [users, totalCount] = await this.userRepository.findAndCount({
+      select: [
+        'id',
+        'userName',
+        'nickName',
+        'email',
+        'phoneNumber',
+        'isFrozen',
+        'headPic',
+        'createTime',
+      ],
+      skip: skipCount,
+      take: pageSize,
+      where: condition,
+    });
+
+    return {
+      users,
+      totalCount,
+    };
+  }
+
+  async delete(userId: number) {
+    //管理员账号不允许删除
+    const user = await this.userRepository.findOneBy({
+      id: userId,
+      isAdmin: false,
+    });
+    if (!user) {
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    }
+
+    try {
+      await this.userRepository.delete(userId);
+      return '删除成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '删除失败';
+    }
   }
 }
